3935be1baae8c0ac0f58d80b8535fd5225e0bda3
MarcoFelipeKing <51195817+MarcoFelipeKing@users.noreply.github.com>
2022-04-29 16:07:28 +0100

    all trajectories added

--- Code/ABCCODE/BethExpt_ABC.py
+++ Code/ABCCODE/BethExpt_ABC.py
@@ -31,7 +31,7 @@ def ode_model(contamination,t,r,C,m,g,l):
 	
 	Contamination = contamination;
 	
-	return(r*(1-Contamination/C)-m*math.exp(-g*t)*Contamination)
+	return((Contamination*r+l)*(1-Contamination/C)-m*math.exp(-g*t)*Contamination)
 
 def deterministic_run(precision,initial_contamination,r,C,m,g,l):
     tmax = 24
@@ -45,10 +45,7 @@ def deterministic_run(precision,initial_contamination,r,C,m,g,l):
     num_at_2=sim[int(precision*2.0/tmax)]
     num_at_4=sim[int(precision*4.0/tmax)]
     num_at_8=sim[int(precision*8.0/tmax)]
-   #num_at_12=sim[int(precision*12.0/tmax)]
     num_at_24=sim[int(precision*24.0/tmax)]
-    #num_at_48=sim[int(precision*48.0/tmax)]
-    
     
     return([num_at_0,num_at_1,num_at_2,num_at_4,num_at_8,num_at_24])
 
@@ -75,18 +72,9 @@ Distilled_SD=[[np.zeros((1,6)) for i in range(1)] for j in range(1)]
 # Detergent for the Locker. Phases 1,2,3. 
 Detergent_Means[0][0] = [221.6,94.3,96.25,1.75,1.6,8.5]
 Detergent_SD[0][0] = [76.4,86.9,89.4,0.5,2.3,4.04]
-# Detergent_Means[0][1] = [5.608333333,4.85,4.25,3.608333333,5.05,3.65,5.05,5.45]
-# Detergent_SD[0][1] = [2.777436282,2.989637275,3.051285766,3.032640915,2.940795107,2.989637275,2.940795107,2.796549598]
-# Detergent_Means[0][2] = [6.875,3.85,3.65,3.05,2.85,3.25,4.25,6.05]
-# Detergent_SD[0][2] = [4.264301495,3.024041598,2.989637275,2.796549598,2.698658671,2.876779809,3.051285766,2.441028613]
 
-# Detergent for Right BR
 Disinfectant_Means[0][0] = [59.4,18.7,5.4,5.4,2.4,8.6]
 Disinfectant_SD[0][0] = [91.8,26.2,2.30,4.67,4.34,4.28] 
-# Detergent_Means[1][1]  = [5.641666667,5.033333333,3.208333333,3.166666667,3.408333333,3.975,5.075,4.991666667]
-# Detergent_SD[1][1]  = [7.479267321,5.002499375,3.102071055,2.9871492,3.417311405,7.372391415,4.960751125,5.058238415]
-# Detergent_Means[1][2]  = [6.875,3.85,3.65,3.05,2.85,3.25,4.25,6.05]
-# Detergent_SD[1][2]  = [4.264301495,3.024041598,2.989637275,2.796549598,2.698658671,2.876779809,3.051285766,2.441028613]
 
 Distilled_Means[0][0] = [261.0,175.5,47,18.6,128,56]
 Distilled_SD[0][0] = [31.5,61.7,9.0,13.2,78.2,76.4]
@@ -94,56 +82,6 @@ Distilled_SD[0][0] = [31.5,61.7,9.0,13.2,78.2,76.4]
 Control_Means[0][0] = [59.4,41,52,18.6,21,16.5]
 Control_SD[0][0] = [23.5,9.9,21.7,13.2,16.2,6.54]
 
-# # Detergent for Table
-# Detergent_Means[2][0] = [13.60833333,10.625,8.391666667,6.433333333,6.9,6.033333333,7.683333333,7.725]
-# Detergent_SD[2][0] = [11.7817985,9.931467753,8.608210796,4.754195062,5.89526037,4.792684799,6.743882924,6.697496959]
-# Detergent_Means[2][1] = [8.325,8.525,5.45,4.65,7.75,5.7,7.683333333,5.275]
-# Detergent_SD[2][1] = [6.911826654,7.23756365,3.051285766,3.204264848,8.958621353,7.453592052,7.192303118,4.925172848]
-# Detergent_Means[2][2] = [8.191666667,5.275,5.675,5.475,6.075,4.45,6.075,8.766666667]
-# Detergent_SD[2][2] = [8.414055871,5.130915425,5.23964496,5.46292362,5.711943023,3.280033642,5.031152949,7.456829778]
-
-# # Detergent for Left BR
-# Detergent_Means[3][0] = [6.058333333,6.525,5.283333333,3.75,3.508333333,1.6,3.925,3.2]
-# Detergent_SD[3][0] = [6.937746578,7.212148515,7.539074458,5.273285798,5.727862182,4.207444142,7.553473168,5.735551562]
-# Detergent_Means[3][1] = [4.175,5.283333333,5.141666667,2.708333333,4.233333333,2.341666667,3.475,3.675]
-# Detergent_SD[3][1] = [6.661202862,8.293848988,7.284992841,4.994170452,5.129781201,5.080939704,5.220855063,5.313166794]
-# Detergent_Means[3][2] = [4.958333333,4.291666667,3.35,2.408333333,2.808333333,2.166666667,2.25,4.566666667]
-# Detergent_SD[3][2] = [8.597501141,6.563318914,5.050264588,2.557359225,2.733965795,2.580597365,3.051285766,3.479620471]
-
-
-# # We store Disinfectant[Surface][Phase]
-
-# # Disinfectant for the Locker. Phases 1,2,3. 
-# Disinfectant_Means[0][0] = [4.275,0.333333333,0.875,1.166666667,1.25,2.65,5.675,2.408333333]
-# Disinfectant_SD[0][0] = [5.031088694,0.562220556,0.5826145,0.317135165,0.72,2.581098403,4.817868144,2.472681196]
-# Disinfectant_Means[0][1] = [4.275,1.466666667,1.075,1.4,4.05,4.25,7.25,4.875]
-# Disinfectant_SD[0][1] = [5.031088694,2.381441657,1.301772664,1.66816025,3.04449758,3.051285766,2.74,4.990832112]
-# Disinfectant_Means[0][2] = [3.95,2.6,2.525,2.85,4.3,3.208333333,2.366666667,3.633333333]
-# Disinfectant_SD[0][2] = [5.114381346,2.892916438,2.677613129,2.698658671,7.306232578,2.915537548,2.503216322,5.023107524]
-
-# # Disinfectant for Right BR
-# Disinfectant_Means[1][0] = [2.408333333,0.783333333,1.691666667,0.958333333,1.608333333,3.3,2.808333333,2.65]
-# Disinfectant_SD[1][0] = [2.472681196,1.370460112,4.787143042,0.537728834,1.550421708,6.354607343,2.733637318,2.581098403]
-# Disinfectant_Means[1][1]  = [4.725,2.058333333,1.475,1.641666667,2.525,2.45,7.25,4.458333333]
-# Disinfectant_SD[1][1]  = [8.315790443,4.844425935,2.038794015,1.95827525,2.677613129,2.441028613,2.74,6.448430487]
-# Disinfectant_Means[1][2]  = [2.441666667,1.716666667,2.083333333,1.883333333,2.483333333,2.05,1.2,1.641666667]
-# Disinfectant_SD[1][2]  = [2.736789062,2.274041417,2.388309651,2.183138098,2.707694443,2.074475422,1.249827574,1.95827525]
-
-# # Disinfectant for Table
-# Disinfectant_Means[2][0] = [5.525,4.158333333,3.35,4.683333333,2.875,2.85,5.433333333,4.433333333]
-# Disinfectant_SD[2][0] = [7.458895985,7.652327608,5.041038481,7.578592051,4.832875028,2.698658671,4.916954015,5.05867867]
-# Disinfectant_Means[2][1] = [3.633333333,4.266666667,2.966666667,2.725,2.85,4.85,7.25,4.5]
-# Disinfectant_SD[2][1] = [5.023107524,5.184082623,2.869078109,2.800361738,2.698658671,2.989637275,2.74,6.422616289]
-# Disinfectant_Means[2][2] = [6.325,3.675,3.075,4.075,3.508333333,3.833333333,2.991666667,3.3]
-# Disinfectant_SD[2][2] = [7.270675039,4.997046542,4.887153286,5.028003477,5.098378433,5.044343594,4.929452011,7.242237223]
-
-# # Disinfectant for Left BR
-# Disinfectant_Means[3][0] = [4.908333333,0.658333333,0.991666667,1.041666667,1.283333333,1.325,2.008333333,1.808333333]
-# Disinfectant_SD[3][0] = [9.233205474,1.379472871,1.328538433,0.473811277,1.206543462,1.182031419,2.103448865,1.858906025]
-# Disinfectant_Means[3][1] = [4.1,1.508333333,2.841666667,2.866666667,2.991666667,2.966666667,7.25,5.308333333]
-# Disinfectant_SD[3][1] = [7.30481655,2.365779136,6.534551185,4.989529266,4.929452011,2.869078109,2.74,9.194658151]
-# Disinfectant_Means[3][2] = [3.15,2,2.775,1.766666667,1.808333333,2.408333333,2.35,3.733333333]
-# Disinfectant_SD[3][2] = [4.999827583,2.438908744,7.215987399,1.88566888,1.858906025,2.472681196,4.745051869,6.502961306]
 
 ##################################################################################################################
 ## Applying the ABC algorithm
@@ -184,7 +122,7 @@ while len(parameter_sample) < sample_size:
     
 	euclidean_distance=0
     
-	delta = 10.0
+	delta = 100.0
     
 	# Learning from data for detergent
 	for surface in range(1):
@@ -527,11 +465,94 @@ plt.ylabel("CFU recovered from coupon \n after t hours")
 plt.yscale("log")
 plt.xlabel("Hours after surface cleaning")
 plt.legend(loc="upper right")
+plt.title("Detergent")
 #make y axis logarithmic
 
 
 #save the plot
-plt.savefig("abc_prediction_BethExpt.png", dpi=600)
+plt.savefig("abc_prediction_BethExpt_detergent.png", dpi=600)
+
+plt.show()
+
+#2. Disinfectant
+initial_contamination=Disinfectant_Means[0][0][0]
+P=odeint(ode_model,initial_contamination,time_space,args=(df["r"].mean(),df["C"].mean(),df["mu-disinfectant"].mean(),df["gamma-disinfectant"].mean(),df["lambda"].mean()))
+Pmin=odeint(ode_model,initial_contamination,time_space,args=(df["r"].quantile(0.05),df["C"].quantile(0.05),df["mu-disinfectant"].quantile(0.05),df["gamma-disinfectant"].quantile(0.05),df["lambda"].quantile(0.05)))
+Pmax=odeint(ode_model,initial_contamination,time_space,args=(df["r"].quantile(0.95),df["C"].quantile(0.95),df["mu-disinfectant"].quantile(0.95),df["gamma-disinfectant"].quantile(0.95),df["lambda"].quantile(0.95)))
+
+# create a vector of values between 0 and 6
+x = np.array([0,2,4,8,12,24])
+
+#Define new sd just for plotting to avoid SD value at 0
+s = Disinfectant_SD[0][0]
+measurement_data = Disinfectant_Means[0][0]#np.array([1200,134.0,202.0,294.0])
+
+#Plot errobars of experimental data
+plt.errorbar(x,measurement_data,yerr=s,fmt='o', color='black',label='Experimental data')
+
+#Plot the model prediction
+plt.plot(time_space,P,label="Model prediction",color='blue')
+
+#Plot confidence intervals around the model prediction
+
+plt.fill_between(time_space,np.concatenate(Pmin),np.concatenate(Pmax),alpha=0.2,color='red')
+#plt.plot(time_space,Pmin,label="Model prediction",color='red')
+#plt.plot(time_space,Pmax,label="Model prediction",color='red')
+
+#plt.fill_between(x, np.array(map(operator.sub, P["Contamination"], Pmin["Contamination"])), np.array(map(operator.add, P["Contamination"], Pmax["Contamination"])), color='b', alpha=.1)
+plt.xlim(-1,25)
+plt.ylabel("CFU recovered from coupon \n after t hours")
+plt.yscale("log")
+plt.xlabel("Hours after surface cleaning")
+plt.legend(loc="upper right")
+#Add title
+plt.title("Disinfectant")
+#make y axis logarithmic
+
+
+#save the plot
+plt.savefig("abc_prediction_BethExpt_disinfectant.png", dpi=600)
+
+plt.show()
+
+#3. Distilled water
+initial_contamination=Distilled_Means[0][0][0]
+P=odeint(ode_model,initial_contamination,time_space,args=(df["r"].mean(),df["C"].mean(),df["mu-distilled-water"].mean(),df["gamma-distilled-water"].mean(),df["lambda"].mean()))
+Pmin=odeint(ode_model,initial_contamination,time_space,args=(df["r"].quantile(0.05),df["C"].quantile(0.05),df["mu-distilled-water"].quantile(0.05),df["gamma-distilled-water"].quantile(0.05),df["lambda"].quantile(0.05)))
+Pmax=odeint(ode_model,initial_contamination,time_space,args=(df["r"].quantile(0.95),df["C"].quantile(0.95),df["mu-distilled-water"].quantile(0.95),df["gamma-distilled-water"].quantile(0.95),df["lambda"].quantile(0.95)))
+
+# create a vector of values between 0 and 6
+x = np.array([0,2,4,8,12,24])
+
+#Define new sd just for plotting to avoid SD value at 0
+s = Distilled_SD[0][0]
+measurement_data = Distilled_Means[0][0]#np.array([1200,134.0,202.0,294.0])
+
+#Plot errobars of experimental data
+plt.errorbar(x,measurement_data,yerr=s,fmt='o', color='black',label='Experimental data')
+
+#Plot the model prediction
+plt.plot(time_space,P,label="Model prediction",color='blue')
+
+#Plot confidence intervals around the model prediction
+
+plt.fill_between(time_space,np.concatenate(Pmin),np.concatenate(Pmax),alpha=0.2,color='green')
+#plt.plot(time_space,Pmin,label="Model prediction",color='red')
+#plt.plot(time_space,Pmax,label="Model prediction",color='red')
+
+#plt.fill_between(x, np.array(map(operator.sub, P["Contamination"], Pmin["Contamination"])), np.array(map(operator.add, P["Contamination"], Pmax["Contamination"])), color='b', alpha=.1)
+plt.xlim(-1,25)
+plt.ylabel("CFU recovered from coupon \n after t hours")
+plt.yscale("log")
+plt.xlabel("Hours after surface cleaning")
+plt.legend(loc="upper right")
+#Add title
+plt.title("Distilled-water")
+#make y axis logarithmic
+
+
+#save the plot
+plt.savefig("abc_prediction_BethExpt_distilled-water.png", dpi=600)
 
 plt.show()
 
